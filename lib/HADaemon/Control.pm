4a698d4301b86c8b3f977da3d6ee2e8603cd56e6
Ivan Kruglov <ivan.kruglov@booking.com>
2013-12-06 22:17:16 +0100

    some working solution

--- lib/HADaemon/Control.pm
+++ lib/HADaemon/Control.pm
@@ -14,8 +14,8 @@ use IPC::ConcurrencyLimit::WithStandby;
 # Accessor building
 my @accessors = qw(
     pid_dir quiet color_map name kill_timeout program program_args
-    stdout_file stderr_file umask directory limit_options
-    stop_file standby_stop_file
+    stdout_file stderr_file umask directory ipc_cl_options
+    standby_stop_file
 );
 
 foreach my $method (@accessors) {
@@ -52,34 +52,34 @@ sub run {
     my ($self) = @_;
 
     # Error Checking.
+    $self->ipc_cl_options
+        or die "Error: ipc_cl_options must be defined\n";
     $self->program && ref $self->program eq 'CODE'
-        or die "Error: program must be defined and must be coderef";
+        or die "Error: program must be defined and must be coderef\n";
     $self->name
-        or die "Error: name must be defined";
+        or die "Error: name must be defined\n";
     $self->pid_dir
-        or die "Error: pid_dir must be defined";
+        or die "Error: pid_dir must be defined\n";
 
     $self->_create_dir($self->pid_dir);
 
-    if ($self->stop_file) {
-        # TODO
-    } else {
-        $self->stop_file(catfile($self->pid_dir, 'stop-file'));
-        $self->standby_stop_file(catfile($self->pid_dir, 'stop-file-standby'));
-    }
+    $self->standby_stop_file
+        or $self->standby_stop_file(catfile($self->pid_dir, 'standby-stop-file'));
+
+    $self->{interval} = int($self->{ipc_cl_options}->{interval} // 0) + 3;
 
     my $called_with = $ARGV[0] // '';
     $called_with =~ s/^[-]+//g;
 
     my $allowed_actions = join('|', $self->_all_actions());
     $called_with
-        or die "Must be called with an action: [$allowed_actions]";
+        or die "Must be called with an action: [$allowed_actions]\n";
 
     my $action = "do_$called_with";
     $self->can($action)
         and exit($self->$action() // 0);
 
-    die "Error: unknown action $called_with. [$allowed_actions]";
+    die "Error: unknown action $called_with. [$allowed_actions]\n";
 }
 
 #####################################
@@ -88,178 +88,115 @@ sub run {
 sub do_start {
     my ($self) = @_;
 
-    $self->_unlink_file($self->stop_file);
     $self->_unlink_file($self->standby_stop_file);
 
-    my $started = 0;
-    my $expected = $self->_expected_processes();
-
-    foreach (1 .. $expected) {
-        $self->_fork() == 0 and ++$started;
-    }
-
-    if ($started == $expected) {
-        $self->pretty_print('', "Started $started/$expected");
+    if ($self->_fork_mains() && $self->_fork_standbys()) {
+        $self->pretty_print('starting main + standby processes', 'OK');
         return 0;
     }
 
-    $self->pretty_print('', 'Failed', 'red');
-    print "Started $started out of $expected processes\n";
+    $self->pretty_print('starting main + standby processes', 'Failed', 'red');
+    $self->do_status();
     return 1;
 }
 
-sub do_status {
+sub do_stop {
     my ($self) = @_;
-    foreach my $type ($self->_expected_processes()) {
-        if ($self->_process_type_is_running($type)) {
-            $self->pretty_print($type, 'Running');
-        } else {
-            $self->pretty_print($type, 'Not Running', 'red');
+
+    $self->_write_file($self->standby_stop_file);
+
+    foreach my $type (reverse $self->_expected_processes()) {
+        my $pid = $self->_pid_of_process_type($type);
+        if ($pid && $self->_kill_pid($pid)) {
+            $self->_unlink_file($self->_build_pid_file($type));
         }
     }
-}
 
-sub _main_running {
-    my ($self) = @_;
-}
+    if ($self->_main_running() == 0 && $self->_standby_running() == 0) {
+        $self->pretty_print('stopping main + standby processes', 'OK');
+        return 0;
+    }
 
-sub _standby_running {
-    my ($self) = @_;
-    my @running = grep { $self->_process_type_is_running($_) } $self->_expected_standby_processes();
-    return wantarray ? @running : scalar @running;
+    $self->pretty_print('stopping main + standby processes', 'Failed', 'red');
+    $self->do_status();
+    return 1;
 }
 
 sub do_restart {
     my ($self) = @_;
-    my $exit_code = 0;
+
+    my @expected_main = $self->_expected_main_processes();
+    my @expected_standby = $self->_expected_standby_processes();
+    my %old_main_pids = map { $_ => $self->_pid_of_process_type($_) } @expected_main;
 
     # stoping standby
     $self->_write_file($self->standby_stop_file);
 
-    my $sleep_interval = 3;
-    my @expected_standby = $self->_expected_standby_processes();
-
-    for (1 .. $sleep_interval) {
-        last if $self->_standby_running() == 0;
-        sleep(1);
+    foreach my $type (@expected_standby) {
+        my $pid = $self->_pid_of_process_type($type);
+        if ($pid && $self->_kill_pid($pid)) {
+            $self->_unlink_file($self->_build_pid_file($type));
+        }
     }
 
-    if ($self->_standby_running() != 0) {
-        $self->pretty_print('stopping standby', 'Failed', 'red');
+    if ($self->_standby_running()) {
+        $self->pretty_print('stopping standby processes', 'Failed', 'red');
+        warn "all standby processes should be stopped at this moment. Can't move forward\n";
         return 1;
     }
 
-    $self->pretty_print('stopping standby', 'OK');
+    $self->pretty_print('stopping standby processes', 'OK');
 
     # starting standby
     $self->_unlink_file($self->standby_stop_file);
 
-    for (1..10) {
-        my @running_standby = $self->_standby_running();
-        my $to_start = scalar @expected_standby - scalar @running_standby;
-        last unless $to_start;
-
-        $self->_fork() foreach (1 .. $to_start);
-        sleep($sleep_interval);
-    }
-
-    if ($self->_standby_running != scalar @expected_standby) {
+    if (!$self->_fork_standbys()) {
         $self->pretty_print('starting standby', 'Failed', 'red');
-        return 1; 
+        warn "all standby processes should be running at this moment. Can't move forward\n";
+        return 1;
     }
 
-    $self->pretty_print('starting standby', 'OK');
+    $self->pretty_print('starting standby processes', 'OK');
 
     # restarting main
-    my @expected_main = $self->_expected_main_processes();
-    my %main_pids = map { $_ => $self->_process_type_is_running($_) } @expected_main;
-
     foreach my $type (@expected_main) {
-        my $pid = $self->_process_type_is_running($type);
-        if ($self->_kill_pid($pid)) {
-            # waiting for promotion of one of standbys
-            my $new_pid = 0;
-            for (1..10) {
-                $new_pid = $self->_process_type_is_running($type); 
-                last if $new_pid;
-                sleep(1);
-            }
-
-            if (not $new_pid) {
-                $self->pretty_print($type, 'Failed to promote standby', 'red');
-            } elsif ($new_pid == $pid) {
-                $self->pretty_print($type, 'Failed to restart', 'red');
-            } else {
-                $self->pretty_print($type, 'Restarted');
-            }
-        } else {
-            $self->pretty_print($type, 'Failed to stop', 'red');
+        my $pid = $self->_pid_of_process_type($type);
+        if (!$pid || $self->_kill_pid($pid)) {
+            $self->_fork_mains();
         }
     }
 
+    # making sure the mains were restarted
+    my $main_running = 0;
+    foreach my $type (keys %old_main_pids) {
+        my $old_pid = $old_main_pids{$type};
+        my $new_pid = $self->_pid_of_process_type($type);
+        next unless $new_pid;
+
+        $main_running++;
+        if ($old_pid && $old_pid == $new_pid) {
+            $self->pretty_print($type, 'Failed to restart', 'red');
+        }
+    }
+
+    if ($main_running != scalar @expected_main) {
+        $self->pretty_print('restarting main', 'Failed', 'red');
+        warn "all main processes should be running at this moment\n";
+        return 1;
+    }
+
+    $self->pretty_print('restarting main processes', 'OK');
+
     # starting standbys again
-    for (1..10) {
-        my @running_standby = $self->_standby_running();
-        my $to_start = scalar @expected_standby - scalar @running_standby;
-        last unless $to_start;
-
-        $self->_fork() foreach (1 .. $to_start);
-        sleep($sleep_interval);
+    if (!$self->_fork_standbys()) {
+        $self->pretty_print('starting standby', 'Failed', 'red');
+        warn "all standby processes should be running at this moment. Can't move forward\n";
+        return 1;
     }
 
-    if ($self->_standby_running == scalar @expected_standby) {
-        $self->pretty_print('starting standby', 'OK');
-    }
-    
-    # starting new standby
-    print "\nSTATUS REPORT\n";
-    $self->do_status();
+    $self->pretty_print('starting standby processes', 'OK');
     return 0;
-
-    # start killing processes from standby
-
-#    NAME:
-#    foreach my $type (reverse $self->_expected_processes()) {
-#        my $pidfile = $self->_build_pid_file($type);
-#        my $pid = $self->_read_pid_file($pidfile);
-#
-#        if ($pid && $self->_pid_running($pid)) {
-#            if ($self->_kill_pid($pid)) {
-#                $self->pretty_print($type, 'Stopped');
-#            } else {
-#                $self->pretty_print($type, 'Failed to stop', 'red');
-#                $exit_code = 1;
-#                next NAME;
-#            }
-#
-#            my $npid = $self->_read_pid_file($pidfile);
-#            if ($npid && $npid == $pid) {
-#                $self->_unlink_file($pidfile);
-#            }
-#        } else {
-#            $self->pretty_print($type, 'Not Running', 'red');
-#            $self->_unlink_file($pidfile);
-#        }
-#    }
-
-    return $exit_code;
 }
-
-#sub do_restart {
-#    my ($self) = @_;
-#
-#    my @standby = map { "standby-$_" } 1 .. $self->_num_of_standby;
-#
-#    foreach my $type (@standby) {
-#        my $pidfile = $self->_build_pid_file($type);
-#        my $pid = $self->_read_pid_file($pidfile);
-#
-#        if ($pid && $self->_pid_running($pid)) {
-#        } else {
-#            $self->pretty_print($type, 'Not Running', 'red');
-#        }
-#    }
-#}
 
 sub do_hard_restart {
     my ($self) = @_;
@@ -267,14 +204,23 @@ sub do_hard_restart {
     return $self->do_start();
 }
 
-sub do_spawn {
+sub do_status {
+    my ($self) = @_;
+    foreach my $type ($self->_expected_processes()) {
+        if ($self->_pid_of_process_type($type)) {
+            $self->pretty_print("$type status", 'Running');
+        } else {
+            $self->pretty_print("$type status", 'Not Running', 'red');
+        }
+    }
+}
+
+sub do_fork {
     my ($self) = @_;
 
     $self->_fork(); # always spawn at least one new process
-    foreach my $type ($self->_expected_processes()) {
-        $self->_process_type_is_running($type)
-            or $self->_fork();
-    }
+    $self->_fork_mains();
+    $self->_fork_standbys();
 
     return 0;
 }
@@ -282,6 +228,48 @@ sub do_spawn {
 #####################################
 # routines to work with processes
 #####################################
+sub _fork_mains {
+    my ($self) = @_;
+    my $expected_main = $self->_expected_main_processes();
+
+    for (1..10) {
+        my $to_start = $expected_main - $self->_main_running();
+        last unless $to_start;
+
+        $self->_fork() foreach (1 .. $to_start);
+        sleep($self->{interval});
+    }
+
+    return $self->_main_running() == $expected_main;
+}
+
+sub _fork_standbys {
+    my ($self) = @_;
+    my $expected_standby = $self->_expected_standby_processes();
+
+    for (1..10) {
+        my $to_start = $expected_standby - $self->_standby_running();
+        last unless $to_start;
+
+        $self->_fork() foreach (1 .. $to_start);
+        sleep($self->{interval});
+    }
+
+    return $self->_standby_running() == $expected_standby;
+}
+
+sub _main_running {
+    my ($self) = @_;
+    my @running = grep { $self->_pid_of_process_type($_) } $self->_expected_main_processes();
+    return wantarray ? @running : scalar @running;
+}
+
+sub _standby_running {
+    my ($self) = @_;
+    my @running = grep { $self->_pid_of_process_type($_) } $self->_expected_standby_processes();
+    return wantarray ? @running : scalar @running;
+}
+
 sub _pid_running {
     my ($self, $pid) = @_;
     my $res = $pid && $pid > 1 ? kill(0, $pid) : 0;
@@ -290,9 +278,8 @@ sub _pid_running {
 }
 
 # rename is pid_of_process_type
-sub _process_type_is_running {
+sub _pid_of_process_type {
     my ($self, $type) = @_;
-
     my $pidfile = $self->_build_pid_file($type);
     my $pid = $self->_read_file($pidfile);
     return $pid && $self->_pid_running($pid) ? $pid : undef;
@@ -397,25 +384,27 @@ sub _launch_program {
     my ($self) = @_;
     $self->trace("_launch_program()");
 
+    return if -f $self->standby_stop_file;
+
     my $pid_file = $self->_build_pid_file("unknown-$$");
     $self->_write_file($pid_file, $$);
     $self->{pid_file} = $pid_file;
 
-    my $limit = IPC::ConcurrencyLimit::WithStandby->new(%{ $self->limit_options });
+    my $ipc = IPC::ConcurrencyLimit::WithStandby->new(%{ $self->ipc_cl_options });
 
     # have to duplicate some logic from IPC::CL:WS
-    my $retries_classback = $limit->{retries};
+    my $retries_classback = $ipc->{retries};
     if (ref $retries_classback ne 'CODE') {
         my $max_retries = $retries_classback;
         $retries_classback = sub { return $_[0] != $max_retries + 1 };
     }
 
-    my $limit_weak = $limit;
-    weaken($limit_weak);
+    my $ipc_weak = $ipc;
+    weaken($ipc_weak);
 
-    $limit->{retries} = sub {
+    $ipc->{retries} = sub {
         if ($_[0] == 1) { # run code on first attempt
-            my $id = $limit_weak->{standby_lock}->lock_id();
+            my $id = $ipc->{standby_lock}->lock_id();
             $self->trace("acquired standby lock $id");
 
             # adjusting name of pidfile
@@ -424,22 +413,25 @@ sub _launch_program {
             $self->{pid_file} = $pid_file;
         }
 
+        return 0 if -f $self->standby_stop_file;
         return $retries_classback->(@_);
     };
 
-    my $id = $limit->get_lock();
+    my $id = $ipc->get_lock();
     if (not $id) {
         $self->_unlink_file($self->{pid_file});
         $self->trace('failed to acquire both locks');
         return 1; # TODO call callback or maybe pass it $self->program
     }
 
-    $self->trace("acquired main lock id: " . $limit->lock_id());
+    $self->trace("acquired main lock id: " . $ipc->lock_id());
     
     # now pid file should be 'main-$id'
     $pid_file = $self->_build_pid_file("main-$id");
     $self->_rename_file($self->{pid_file}, $pid_file);
     $self->{pid_file} = $pid_file;
+
+    return 0 if -f $self->standby_stop_file;
 
     my @args = @{ $self->program_args // [] };
     my $res = $self->program->($self, @args);
@@ -513,7 +505,7 @@ sub pretty_print {
 
     local $| = 1;
     $process_type =~ s/-/ #/;
-    printf("%s %-40s %40s\n", $self->name, $process_type, "\033[$code" ."m[$message]\033[0m");
+    printf("%s: %-40s %40s\n", $self->name, $process_type, "\033[$code" ."m[$message]\033[0m");
 }
 
 sub trace {
@@ -532,14 +524,14 @@ sub _all_actions {
 
 sub _expected_main_processes {
     my ($self) = @_;
-    my $num = $self->{limit_options}->{max_procs} // 0;
+    my $num = $self->{ipc_cl_options}->{max_procs} // 0;
     my @expected = map { "main-$_" } ( 1 .. $num );
     return wantarray ? @expected : scalar @expected;
 }
 
 sub _expected_standby_processes {
     my ($self) = @_;
-    my $num = $self->{limit_options}->{standby_max_procs} // 0;
+    my $num = $self->{ipc_cl_options}->{standby_max_procs} // 0;
     my @expected = map { "standby-$_" } ( 1 .. $num );
     return wantarray ? @expected : scalar @expected;
 }
--- test.pl
+++ test.pl
@@ -8,7 +8,7 @@ HADaemon::Control->new({
     name => 'test.pl',
     pid_dir => '/tmp/test',
     program => sub { sleep 10 },
-    limit_options => {
+    ipc_cl_options => {
         max_procs => 1,
         standby_max_procs => 2,
         path => '/tmp/test/lock',
